530cf81022e76ffea4ce4503ac372ccc82cd21bd
cfmcginn <cfmcginn@mit.edu>
2014-09-19 14:49:55 -0400

    Added Justins DelR header

---
+++ getDelR.h
@@ -0,0 +1,25 @@
+#include <vector>
+#include "fastjet/JetDefinition.hh"
+#include "fastjet/ClusterSequence.hh"
+#include "fastjet/PseudoJet.hh"
+
+Float_t getAvgDelR(fastjet::PseudoJet inJt, Float_t ptCut, Float_t ptWeight)
+{
+  Float_t delR = -1;
+  
+  if(inJt.perp() > ptCut){ 
+    std::vector<fastjet::PseudoJet> constituents = inJt.constituents();
+    Float_t R_sum = 0.0;
+    Float_t pt_sum = 0.0;
+
+    for(Int_t j = 0; j < constituents.size(); j++){      
+      Float_t distance = getDR(inJt.eta(), inJt.phi(), constituents[j].eta(), constituents[j].phi());
+      R_sum += distance*pow(constituents[j].perp(), ptWeight);
+      pt_sum += pow(constituents[j].perp(), ptWeight);
+    }
+
+    delR = R_sum/pt_sum;
+  }
+  
+  return delR;
+}
