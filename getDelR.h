4245257fe8eafb65e188c93a7417b0a84d30b908
cfmcginn <cfmcginn@mit.edu>
2014-09-19 15:48:56 -0400

    yolo

--- getDelR.h
+++ getDelR.h
@@ -14,7 +14,7 @@ Float_t getAvgDelR(fastjet::PseudoJet inJt, Float_t ptCut, Float_t ptWeight)
     Float_t pt_sum = 0.0;
 
     for(Int_t j = 0; j < constituents.size(); j++){      
-      Float_t distance = getDR(inJt.eta(), inJt.phi(), constituents[j].eta(), constituents[j].phi());
+      Float_t distance = getDR(inJt.eta(), inJt.phi_std(), constituents[j].eta(), constituents[j].phi_std());
       R_sum += distance*pow(constituents[j].perp(), ptWeight);
       pt_sum += pow(constituents[j].perp(), ptWeight);
     }
