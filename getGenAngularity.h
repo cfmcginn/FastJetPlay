f2b6e389f4ae87c0d6090f6ff6ae04f22440b59d
cfmcginn <cfmcginn@mit.edu>
2014-10-11 12:20:42 -0400

    small error genAng fix

--- getGenAngularity.h
+++ getGenAngularity.h
@@ -10,7 +10,7 @@ Float_t getGenAngularity(fastjet::PseudoJet *inJt, Float_t jtR = 0.3, Float_t pt
 {
   Float_t genAngularity = 0;
 
-  std::vector<fastjet::PseudoJet> jtConst = inJt->constitents();
+  std::vector<fastjet::PseudoJet> jtConst = inJt->constituents();
 
   for(Int_t iter = 0; iter < (Int_t)(jtConst.size()); iter++){
     Float_t constDelR = getDR(jtConst[iter].eta(), jtConst[iter].phi_std(), inJt->eta(), inJt->phi_std());
